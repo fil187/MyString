4bd60d395297c74cf14b76efcb80c2e60397f283
fil187 <vermeire02@outlook.com>
2026-08-01 15:10:14 +0200

    added tests

--- my_string.hpp
+++ my_string.hpp
@@ -139,6 +139,7 @@ public:
     void operator=(const MyString& source) {
         if (this == &source)
             return;
+        
         char* old_data = data;
         data = new char[source.capacity];
         std::copy(source.data, source.data + source.size, data);
--- tests.cpp
+++ tests.cpp
@@ -3,8 +3,8 @@
 #include "my_string.hpp"
 
 void testDefaultConstructor() {
-    MyString str1;
-    assert(str1.length() == 0);
+    MyString str;
+    assert(str.length() == 0);
     std::cout << "default constructor tests passed\n";
 }
 
@@ -27,7 +27,14 @@ void testConstructorFromString() {
     MyString str("hello world!");
     assert(str.length() == 12);
     assert(str == "hello world!");
-    std::cout << "constructor from specified string passed!\n";
+    std::cout << "constructor from specified string passed\n";
+}
+
+void testConstructorEmptyString() {
+    MyString str("");
+    assert(str.length() == 0);
+    assert(str == "");
+    std::cout << "constructor from empty string test passed\n";
 }
 
 void testCopyConstructor() {
@@ -51,37 +58,64 @@ void testCopyConstructorIndependent() {
 void testAddString() {
     MyString str("hello");
     assert(str + " world!" == "hello world!");
+    assert(str == "hello");
     assert((str + " world!").length() == 12);
     std::cout << "string addition test passed\n";
 }
 
-void testAddStringWithResize() {
+void testAddStringWithSingleResize() {
     MyString str(10);
     str += "hello world! this text will resize str.data";
     assert(str.length() == 43);
     assert(str == "hello world! this text will resize str.data");
-    std::cout << "test add triggers resize test passed\n";
+    std::cout << "test add triggers a single resize test passed\n";
 }
 
-void testEqString() {
+void testAddStringWithMultipleResizes() {
+    MyString str(2);
+    str += "hello"; // size and capacity are 5 here
+    str += " world!"; // size and capacity are 12 here
+    str += "this text will resize str.data"; // size and capacity are 43 here
+    std::cout << "test add triggers multiple resize test passed\n";
+}
+
+void testStringAssignment() {
     MyString str;
     assert(str.length() == 0);
     str = "hello world!";
     assert(str.length() == 12);
     assert(str == "hello world!");
-    std::cout << "string eq test passed\n";
+    std::cout << "string Assignment test passed\n";
 }
 
-void testEqMyString() {
+void testAssignmentFromEmptyString() {
+    MyString str("hello world!");
+    str = "";
+    assert(str.length() == 0);
+    assert(str == "");
+    std::cout << "Assignment from empty string test passed\n";
+}
+
+void testMyStringAssignment() {
     MyString str1("hello world!");
     MyString str2("hallo werlde");
     assert(str1 != str2);
     str1 = str2;
     assert(str1 == str2);
-    std::cout << "MyString Eq test passed\n";
+    std::cout << "MyString Assignment test passed\n";
 }
 
-void testEqSelf() {
+void testAssignmentIndependence() {
+    MyString str1("hello world!");
+    MyString str2;
+    str2 = str1;
+    assert(str1 == str2);
+    str1 = "hallo werlde!";
+    assert(str1 != str2);
+    std::cout << "Assignment independent test passed\n";
+}
+
+void testSelfAssignment() {
     MyString str1("hello world!");
     str1 = str1;
     assert(str1 == "hello world!");
@@ -94,6 +128,16 @@ void testConcatination() {
     assert(str.length() == 12);
     assert(str == "hello world!");
     std::cout << "string concatination test passed\n";
+}
+
+void testConcatintionWithEmptyString() {
+    assert(MyString("hello") + "" == "hello");
+    std::cout << "concatination with empty string test passed\n";
+}
+
+void testConcatinationFromEmptyString() {
+    assert(MyString("") + "" == "");
+    std::cout << "concatination from empty string test passed\n";
 }
 
 void testAtIndex() {
@@ -114,24 +158,39 @@ void testIndexOutOfBounds() {
     }
 }
 
+void testIndexJustOutOfBounds() {
+    try {
+        MyString("hello world!")[12];
+        assert(false);
+    } catch (std::out_of_range&) {
+        std::cout << "just out of bounds exception test passed\n";
+    }
+}
+
 void testStartsWith() {
-    MyString str("hello world!");
-    assert(!str.start_with("hallo"));
-    assert(str.start_with("hello"));
+    assert(!MyString("hello world!").start_with("hallo"));
+    assert(MyString("hello world!").start_with("hello"));
     std::cout << "starts with test passed\n";
 }
 
-void testContains() {
-    MyString str("hello world!");
-    assert(!str.contains("hallo"));
-    assert(str.contains("hello"));
-    assert(MyString("").contains(""));
-    assert(!MyString("").contains("a"));
+void testBasicContains() {
+    assert(!MyString("hello world!").contains("hallo"));
+    assert(MyString("hello world!").contains("hello"));
     assert(MyString("abababa").contains("ababa"));
     assert(MyString("abababa").contains("babab"));
+    std::cout << "basic contains test passed\n";
+}
+
+void testEmptyStringContains() {
+    assert(MyString("").contains(""));
+    assert(!MyString("").contains("a"));
+    std::cout << "empty string contains test passed\n";
+}
+
+void testComplexStringContains() {
     assert(MyString("wdYD^\\YmfX`B_mIhQovpLJdEc[XXT\\URX^iaFShNQDYsgAtWlD^ZL\\[YtUXiLfL^MWiQXIuvJTjaOfkk]Th[A[juPaMBwvNWVXQSdyhHaG\\NhGCFUrKrgpxYHRwpxJPkbOpancjbjdhspyXVZicgIK^`CD\\sxWM_lqT]qnO_Yn^GOqYVZWmcTkYKrsOlxne_PzvG[CDTveWI[N^pcAOHAbBcTWilVVXD]hAozPRr[KwCVF`_mL^UBKIxXnWL[ICuVR\\olH]wldUGcHRxqZYbFUqr_qltBWEMC`z[suMYDA[KxEfnNG^DFIzFHdnNWCQH\\Z_PkktdSCAPXCiROojagyE^vXVqShDPgnnEaKRSN^PzrqteCVuby`njwVsCBrPxhnLnYxzRpFqHLomYoEmbnlbBqzIKSnQqnz_CtnGSDsfa]hfNnkivXhJCgtXUgtdIrVPFSKD_apQC[nk_WTSM\\vrcZMNMslOzGloWRzdCQpFM]sMBeP]d").contains("gnnEaKRSN^PzrqteCVuby`njwVsCBrPxhnLnYxzRpFqHLomYoEmbnlbBqzIKSnQqnz_CtnGSDsfa]hfNnkivXhJCgtXUgtdIrVPFSKD_apQC[nk_WTSM\\vr"));
     assert(!MyString("wdYD^\\YmfX`B_mIhQovpLJdEc[XXT\\URX^iaFShNQDYsgAtWlD^ZL\\[YtUXiLfL^MWiQXIuvJTjaOfkk]Th[A[juPaMBwvNWVXQSdyhHaG\\NhGCFUrKrgpxYHRwpxJPkbOpancjbjdhspyXVZicgIK^`CD\\sxWM_lqT]qnO_Yn^GOqYVZWmcTkYKrsOlxne_PzvG[CDTveWI[N^pcAOHAbBcTWilVVXD]hAozPRr[KwCVF`_mL^UBKIxXnWL[ICuVR\\olH]wldUGcHRxqZYbFUqr_qltBWEMC`z[suMYDA[KxEfnNG^DFIzFHdnNWCQH\\Z_PkktdSCAPXCiROojagyE^vXVqShDPgnnEaKRSN^PzrqteCVuby`njwVsCBrPxhnLnYxzRpFqHLomYoEmbnlbBqzIKSnQqnz_CtnGSDsfa]hfNnkivXhJCgtXUgtdIrVPFSKD_apQC[nk_WTSM\\vrcZMNMslOzGloWRzdCQpFM]sMBeP]d").contains("MvFftnypaKrWN\\PpIpxkeuFoWvOF[fjRSYUVNqvm]^_cuGZdsdKu`FOQTs`nXYqMZso\\ei`sAZ`qE`PMtxkQUHDFXNkBFxZUibvj"));
-    std::cout << "conatins test passed\n";
+    std::cout << "complex string contains test passed\n";
 }
 
 int main() {
@@ -142,15 +201,23 @@ int main() {
     testCopyConstructor();
     testCopyConstructorIndependent();
     testAddString();
-    testAddStringWithResize();
-    testEqString();
-    testEqSelf();
+    testAddStringWithSingleResize();
+    testAddStringWithMultipleResizes();
+    testStringAssignment();
+    testAssignmentFromEmptyString();
+    testAssignmentIndependence();
+    testSelfAssignment();
     testConcatination();
+    testConcatintionWithEmptyString();
+    testConcatinationFromEmptyString();
     testAtIndex();
     testIndexOutOfBounds();
-    testEqMyString();
+    testIndexJustOutOfBounds();
+    testMyStringAssignment();
     testStartsWith();
-    testContains();
+    testBasicContains();
+    testEmptyStringContains();
+    testComplexStringContains();
     std::cout << "all tests passed!\n";
     return 0;
 }
